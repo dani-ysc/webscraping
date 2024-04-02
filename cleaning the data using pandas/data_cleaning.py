53a7399a104625a5e3c2cf11e6578d02199caf30
unknown <danielgyoi@gmail.com>
2024-04-02 09:50:53 -0300

    Added modifications in data cleaning

--- cleaning the data using pandas/data_cleaning.py
+++ cleaning the data using pandas/data_cleaning.py
@@ -1,11 +1,20 @@
 # Importar as bibliotecas necessárias para a limpeza dos dados
 import pandas as pd
+import os
+
+path = "E:\Documentos\IT\Repositório\webscraping with python\webscraping\webscraping\dados_2023-10-27.csv"
+output_path = 'cleaned_data'
+
+# Criando uma pasta para salvar os dados limpos
+try:
+    os.mkdir(f'./{output_path}')
+except OSError as error:
+    print(error)
 
 # Importar a planilha com os dados scrapados
-data = pd.read_csv("E:\Documentos\IT\Repositório\dados.csv")
+data = pd.read_csv(path)
 
 # Verificar qual o tipo de dado de cada coluna do DataFrame
-print(data.info())
 print(data.dtypes)
 
 # Retirar o símbolo 'R$' das colunas de preço à vista
@@ -14,14 +23,16 @@ data['preço à vista'] = data['preço à vista'].str.strip('R$')
 # Retirar o símbolo 'R$' das colunas de preço à prazo
 data['preço à prazo'] = data['preço à prazo'].str.strip('R$')
 
-print(data.head())
 # Transformar as colunas contendo preços para float
-data['preço à vista'] = data['preço à vista'].astype('float')
-data['preço à prazo'] = data['preço à prazo'].astype('float')
+data['preço à vista'] = data['preço à vista'].astype('float64')
+data['preço à prazo'] = data['preço à prazo'].astype('float64')
 
-# Verificando se a transformação foi bem sucedida
-print("Verificando se as colunas se transformaram em float")
-assert data['preço à vista'].dtype == 'float'
-assert data['preço à prazo'].dtype == 'float'
+print(data.info())
 
-# Salvar os dados limpos num arquivo .csv 
+# Renomeando colunas para manter um padrão
+data.rename(columns={'preço à vista': 'preco_a_vista', 'preço à prazo': 'preco_a_prazo'})
+
+print(data.head())
+
+# Salvar os dados limpos num arquivo .csv
+data.to_csv(f'.\{output_path}\dados_2023-10-27.csv')
